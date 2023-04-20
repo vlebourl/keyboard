7396aadb6208801448391aa4d647f515d153b5c8
vlebourl <vlebourl@gmail.com>
2023-04-20 18:42:07 +0200

    decrease brightness

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ LED_COUNT = 10  # Number of LED pixels.
 LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 40  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
