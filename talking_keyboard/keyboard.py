afcc080a12555cfdc1addbba3dfd73c3555bf278
Vincent Le Bourlot <vlebourl@gmail.com>
2024-11-24 10:04:52 +0100

    re-add evdev for linux withouth x server.

--- talking_keyboard/keyboard.py
+++ talking_keyboard/keyboard.py
@@ -5,53 +5,112 @@ from pynput import keyboard
 
 _LOGGER = logging.getLogger(__name__)
 
+if KB_UTIL == "pynput":
 
-class Keyboard:
-    def __init__(self):
-        self.shift_pressed = False
-        self.caps_lock = False
-        self.current_letter = None
+    from pynput import keyboard
+    from const import KEY_MAP, DIGITS_MAP
 
-        # Listener for keyboard events
-        self.listener = keyboard.Listener(
-            on_press=self.on_press, on_release=self.on_release
-        )
-        self.listener.start()
+    class Keyboard:
+        def __init__(self):
+            self.shift_pressed = False
+            self.caps_lock = False
+            self.current_letter = None
 
-    def on_press(self, key):
-        try:
-            if key == keyboard.Key.shift or key == keyboard.Key.shift_r:
-                self.shift_pressed = True
-            elif key == keyboard.Key.caps_lock:
+            # Listener for keyboard events
+            self.listener = keyboard.Listener(
+                on_press=self.on_press, on_release=self.on_release
+            )
+            self.listener.start()
+
+        def on_press(self, key):
+            try:
+                if key == keyboard.Key.shift or key == keyboard.Key.shift_r:
+                    self.shift_pressed = True
+                elif key == keyboard.Key.caps_lock:
+                    self.caps_lock = not self.caps_lock
+            except Exception as e:
+                _LOGGER.error(f"Error processing key press: {e}")
+
+        def on_release(self, key):
+            try:
+                # Check if a key maps to a character
+                if hasattr(key, "char") and key.char is not None:
+                    # Adjust character based on shift and caps lock states
+                    char = key.char
+                    if char in DIGITS_MAP:
+                        char = DIGITS_MAP[char]
+                    if self.caps_lock ^ self.shift_pressed:  # XOR for toggling case
+                        char = char.upper() if char.islower() else char.lower()
+                    self.current_letter = char
+                elif hasattr(key, "name") and key.name in KEY_MAP:
+                    # Handle special keys
+                    self.current_letter = KEY_MAP[key.name]
+                else:
+                    _LOGGER.warning("Unsupported key: %s", key)
+
+                if key == keyboard.Key.shift or key == keyboard.Key.shift_r:
+                    self.shift_pressed = False
+            except Exception as e:
+                _LOGGER.error(f"Error processing key release: {e}")
+
+        def get_one_letter(self) -> str:
+            """Blocking call to return the next typed letter."""
+            self.current_letter = None
+            while self.current_letter is None:
+                pass  # Wait for a key to be released and processed
+            return self.current_letter
+
+elif KB_UTIL == "evdev":
+
+    import glob
+
+    from evdev import InputDevice, categorize, ecodes
+    from const import EV_MAP
+
+    class Keyboard:
+
+        def __init__(self):
+            _device_paths = (
+                glob.glob("/dev/input/by-id/*kbd*")
+                or glob.glob("/dev/input/by-id/*ogitech*")
+                or glob.glob("/dev/input/by-id/*keyboard*")
+            )
+            if not _device_paths:
+                raise ValueError("No keyboard device found!")
+            self.device = InputDevice(_device_paths[1])
+            self.shift_pressed = False
+            self.caps_lock = False
+
+        def update_key_states(self, key_event):
+            if key_event.keycode in ["KEY_LEFTSHIFT", "KEY_RIGHTSHIFT"]:
+                self.shift_pressed = key_event.keystate == key_event.key_down
+            elif (
+                key_event.keycode == "KEY_CAPSLOCK"
+                and key_event.keystate == key_event.key_down
+            ):
                 self.caps_lock = not self.caps_lock
-        except Exception as e:
-            _LOGGER.error(f"Error processing key press: {e}")
 
-    def on_release(self, key):
-        try:
-            # Check if a key maps to a character
-            if hasattr(key, "char") and key.char is not None:
-                # Adjust character based on shift and caps lock states
-                char = key.char
-                if char in DIGITS_MAP:
-                    char = DIGITS_MAP[char]
-                if self.caps_lock ^ self.shift_pressed:  # XOR for toggling case
-                    char = char.upper() if char.islower() else char.lower()
-                self.current_letter = char
-            elif hasattr(key, "name") and key.name in KEY_MAP:
-                # Handle special keys
-                self.current_letter = KEY_MAP[key.name]
-            else:
-                _LOGGER.warning("Unsupported key: %s", key)
-
-            if key == keyboard.Key.shift or key == keyboard.Key.shift_r:
-                self.shift_pressed = False
-        except Exception as e:
-            _LOGGER.error(f"Error processing key release: {e}")
-
-    def get_one_letter(self) -> str:
-        """Blocking call to return the next typed letter."""
-        self.current_letter = None
-        while self.current_letter is None:
-            pass  # Wait for a key to be released and processed
-        return self.current_letter
+        def get_one_letter(self):
+            for event in self.device.read_loop():
+                if event.type != ecodes.EV_KEY:
+                    continue
+                key_event = categorize(event)
+                self.update_key_states(key_event)
+                if key_event.keystate == key_event.key_up:
+                    try:
+                        if mapped_key := EV_MAP.get(
+                            (
+                                key_event.keycode[0]
+                                if isinstance(key_event.keycode, list)
+                                else key_event.keycode
+                            ),
+                            "",
+                        ):
+                            return mapped_key
+                        else:
+                            _LOGGER.warning("Unsupported key: %s", key_event.keycode)
+                    except TypeError as e:
+                        _LOGGER.error(
+                            "Error processing key: %s", str(key_event.keycode)
+                        )
+                        _LOGGER.error(e)
